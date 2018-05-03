3a952d783a3830c957123ae031339e159b993a0e
Sandy Edwards <peterjamesedwards@gmail.com>
2018-05-03 15:36:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
   return obj;
 }
 
+function deleteFromObjectByKey(obj, key) {
+var newObj = Object.assign({}, obj);
+delete newObj[key];
+return newObj;
+}
